c18faa326e6a85cd44b19d5c141f3e43d542e2b4
Zhong Lufan <lufanzhong@gmail.com>
2022-10-17 23:05:32 +0800

    Add Bilibili Comment User Location

---
+++ bilibili-comment-user-location.user.js
@@ -0,0 +1,80 @@
+// ==UserScript==
+// @name        Bilibili Comment User Location
+// @namespace   Hill98
+// @description 哔哩哔哩网页版评论区显示用户 IP 归属地
+// @version     1.0.0
+// @author      Hill-98
+// @license     MIT
+// @icon        https://www.bilibili.com/favicon.ico
+// @downloadURL https://github.com/Hill-98/userscripts/raw/main/bilibili-comment-user-location.user.js
+// @homepageURL https://github.com/Hill-98/userscripts
+// @supportURL  https://github.com/Hill-98/userscripts/issues
+// @updateURL   https://github.com/Hill-98/userscripts/raw/main/bilibili-comment-user-location.user.js
+// @grant       none
+// @match       https://www.bilibili.com/*
+// @run-at      document-start
+// ==/UserScript==
+
+const LOG_PREFIX = '[bcul]: ';
+
+const console = Object.assign({}, Object.getOwnPropertyDescriptors(window.console));
+
+const addLocationToReply = function addLocationToReply(id, userid, location) {
+  const el = document.createElement('span');
+  el.classList.add('reply-location')
+  el.style.marginLeft = '18px';
+  el.textContent = location;
+  const containers = document.querySelectorAll(`[data-root-reply-id="${id}"][data-user-id="${userid}"]`);
+  containers.forEach((container) => {
+    let parentElement = container.parentElement;
+    const isSub = parentElement.classList.toString().includes('sub-');
+    if (isSub) {
+      parentElement = parentElement.parentElement;
+    }
+    const info = parentElement.querySelector(isSub ? '.sub-reply-info' : '.reply-info');
+    if (info && !info.querySelector('.reply-location')) {
+      info.append(el);
+    }
+  });
+}
+
+const handleReplies = function handleReplies(replies) {
+  replies.forEach((reply) => {
+    const userid = reply.mid;
+    const rootid = reply.root === 0 ? reply.rpid : reply.root;
+    const control = reply.reply_control;
+    if (control?.location) {
+      addLocationToReply(rootid, userid, control.location);
+    }
+    if (reply.replies) {
+      handleReplies(reply.replies);
+    }
+  });
+};
+
+const handleResponse = function handleResponse(url, response) {
+  if (!url.startsWith('https://api.bilibili.com/x/v2/reply')) {
+    return;
+  }
+  try {
+    const json = JSON.parse(response);
+    if (json.code === 0 && json.data?.replies) {
+      setTimeout(handleReplies.bind(this, json.data.replies), 1000);
+    }
+  } catch (ex) {
+    console.error(LOG_PREFIX, ex);
+  }
+};
+
+window.XMLHttpRequest = class XMLHttpRequestHacker extends XMLHttpRequest {
+  constructor() {
+    super();
+    this.addEventListener('readystatechange', this.hackResponse);
+  }
+
+  hackResponse() {
+    if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+      handleResponse(this.responseURL, this.response);
+    }
+  }
+};

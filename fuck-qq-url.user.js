3fcf6a0df9a13de87f60db816e30e57dce73982c
Zhong Lufan <lufanzhong@gmail.com>
2023-10-07 00:48:44 +0800

    Fuck QQ URL: Update

--- fuck-qq-url.user.js
+++ fuck-qq-url.user.js
@@ -2,7 +2,7 @@
 // @name        Fuck QQ URL
 // @namespace   Hill98
 // @description Skip Unsafe Link Warning in Desktop QQ Client
-// @version     1.2.0
+// @version     1.2.1
 // @author      Hill-98
 // @license     MIT
 // @icon        https://qzonestyle.gtimg.cn/qzone/qzact/act/external/tiqq/logo.png
@@ -12,21 +12,22 @@
 // @updateURL   https://github.com/Hill-98/userscripts/raw/main/fuck-qq-url.user.js
 // @grant       none
 // @match       https://c.pc.qq.com/middlem.html?*
+// @match       https://c.pc.qq.com/pc.html?*
 // ==/UserScript==
 
 const usp = new URLSearchParams(location.search);
-let pfurl = usp.get('pfurl');
-if (pfurl !== null) {
-  pfurl = pfurl.match(/^https?:\/\//) === null ? `http://${pfurl}` : pfurl;
+let url = usp.get('pfurl') || usp.get('url');
+if (url !== null) {
+  url = url.match(/^https?:\/\//) === null ? `http://${url}` : url;
   const meta = document.createElement('meta');
-  meta.content = `0; url='${pfurl}'`;
+  meta.content = `0; url='${url}'`;
   meta.httpEquiv = 'refresh';
   document.head.innerHTML = meta.outerHTML;
   const tips = document.createElement('p');
   tips.style.fontSize = '2rem';
   tips.textContent = 'If there is no jump. ';
   const tipsLink = document.createElement('a');
-  tipsLink.href = pfurl;
+  tipsLink.href = url;
   tipsLink.textContent = 'click here';
   tips.append(tipsLink);
   document.body.innerHTML = tips.outerHTML;
